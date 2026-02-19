f2718b2f9bfc870a3a9d0e9cf20afff239397ed1
Subash <sapkotasubash784@gmail.com>
2026-02-20 00:01:59 +0100

    First session Theory!
    Brief Introduction of Python with History and basics.

---
+++ Session 1/Session 2/d2.py
@@ -0,0 +1,51 @@
+"""
+Versions of Python:
+Identifiers(variables)
+python is case sensitive 
+underscore can be used in naming 
+spacing is not allowed 
+
+reserved words or key words cannot be used as identifiers 
+
+_a private variable
+_ _a protected/strongly private variable
+_ _ a_ _ Magic variables or Dunder Variables 
+
+
+Reserved words/ Key Words 
+ Mainly 33 Reserved words (True False and or not is if , elif, else, while, for, break, continue return, in, yield , async, try ,except, finally, raise assert, import ,from ,as ,class ,def, pass, global nonlocal, import, as, class ,def ,pass ,global, non local, lamda, del ,with  )
+
+"""
+
+""" Data Types """
+a= 10
+print(a)
+print(type(a))
+a=10.5
+print(a)
+print(type(a))
+
+"""
+Python contains 14 data types:
+int , float, complex, bool, str, byte, bytearray, range , list , tuple, set, frozenset, dict, none
+
+
+
+
+
+"""
+
+b=0x103BEEf   #Hexadecimal number representation can be done in variable by putting 0x in front 
+print(b)
+print(type(b))
+
+#This gives output in binary but if we want it in same number system 
+
+c=0xface #Hexadecimal number 
+print(bin(c))# we want output in binary 
+print(oct(c))#we want output in octal 
+ 
+print(c)  #by default every output is in decimal 
+
+e=1.2E3   #1.2*10 power 3 is represented in such way 
+print(e)
--- Session 1/intro.py
+++ Session 1/intro.py
@@ -1,2 +1,81 @@
 print('Hello world')
 """ Here we will be learning about the brief introduction of python and django in this file. """
+
+print('Thanks')
+print('End')
+
+
+
+""" Theory"""
+
+
+
+"""Python Is a General Purpose High Level Programming Language 
+Founded by Guido van Rossum 
+
+
+Feb 20th 1991 python officially launched
+
+Python is Dynamically typed 
+For example: If you have to define something in C programming you must specify what you are defining with variable lets say, int for integers, float for floating point numbers and str for string. 
+
+But, in python you can directly call a=10 and b=20 without explicitly mentioning it to be integer as int as in C programming language. 
+
+
+Most of the syntax of python is similar to C and ABC language.   """
+
+
+"""
+Where can we use python?
+Desktop applications/Standalone application 
+Web applications (django, flask, web2py )
+Database Applications 
+Network Programming 
+Games 
+Data Analytics, Machine Learning, Artificial Intelligence, IOT, Microcontroller and so on 
+
+"""
+
+"""
+Google, Youtube, NASA, Microsoft all of these big companies use python.. and this list goes on and on.....
+
+"""
+
+"""
+Features of Python 
+Simple and easy to learn 
+Freeware and open source 
+High Level Programming language 
+Platform Independent 
+portability(windows code runs easily in mac)
+dynamically typed 
+Interpreted(line by line execution of code(python virtual machine run in background))
+Extensible (it can be used inside other programming language as well)
+Embedded( it can also communicate at hardware level, for example it can be used in washing machine, Ac and other devices)
+
+Extensive Library 
+Huge community 
+
+
+
+"""
+
+"""
+Limitations: 
+Slower than C and C++
+
+
+"""
+
+"""
+Flavors of python 
+Cpython
+Jython or Jpython
+Iron Python 
+Rubypython 
+Anaconda Python 
+
+
+"""
+
+print('First Session Theory Done')
