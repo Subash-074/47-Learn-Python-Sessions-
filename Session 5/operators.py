90e9ecfcfc3f99bd4f7c7416e45c5a6dc2b3f48e
Subash <sapkotasubash784@gmail.com>
2026-02-22 00:28:28 +0100

    ....

---
+++ Session 5/operators.py
@@ -0,0 +1,25 @@
+#Operators are the symbols which performs certain operations. 
+
+#There are multiple operators in python 
+
+"""
+Arithmetic operator 
+Relational Operators/Comparison Operators 
+Logical operators 
+Bitwise Operators 
+Assignment Operators 
+Equality Operators 
+Shift Operators 
+Ternary Operators 
+Special Operators 
+
+
+(Special Operators )
+Identity Operators 
+Membership Operators 
+
+
+"""
+
+#Arithmetif Operators 
+# +, -, *, %, //, **, 
---
+++ Session 5/scapeseq.py
@@ -0,0 +1,31 @@
+"""
+Scape Seq 
+
+if we need print ('python's classes') this produces error in output 
+
+
+"""
+print('python\'s classes')
+
+#This is usefule when we want to change line of output as well.
+
+print('python\n class')
+
+#\n === new line
+#\t === tab 
+#\r=== carsiage return 
+#\b=== backspace
+#\' === single quote 
+#\"=== double quote 
+#\\=== single backslash
+
+#comments in python can be done using triple quotes however comments using triple quote is doc string and readed by python interpretor  # symbol is basically good for comments 
+
+
+
+
+
+
+
+
+
